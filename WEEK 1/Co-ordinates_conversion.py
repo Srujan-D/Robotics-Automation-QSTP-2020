9034051ee17f790a4df34e4765d5d4372207f206
Srujan-D <53463585+Srujan-D@users.noreply.github.com>
2020-05-16 17:36:42 +0530

    Corrected the cart2pol function.
    Earlier code only worked for Quadrants 1 and 4. It is now updated.

--- WEEK 1/Co-ordinates_conversion.py
+++ WEEK 1/Co-ordinates_conversion.py
@@ -7,7 +7,7 @@ def pol2cart(r, theta):         #function for converting polar to cartesian coor
 
 def cart2pol(x, y):             #function for converting cartesian to polar coordinates.
 	r = np.sqrt( x**2 + y**2 )
-	theta = np.arctan(y/x)
+	theta = np.arctan2(y, x)
 	return (r,theta)
 
 
