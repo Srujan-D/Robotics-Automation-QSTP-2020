2b445f71dc557a05865875ad3a229e86c931dd9d
Srujan-D <53463585+Srujan-D@users.noreply.github.com>
2020-05-25 15:28:15 +0530

    Create first_name_pub.py
    publishes my first name on topic ​ /name​.

---
+++ WEEK 2/Solutions/Task 1/scripts/first_name_pub.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def firstname_pub():
+    pub = rospy.Publisher('name', String, queue_size=10)
+    rospy.init_node('first_name_pub', anonymous = True)
+    rate = rospy.Rate(0.1) #10 hz
+
+    while not rospy.is_shutdown():
+        fname = "Srujan"
+        rospy.loginfo(fname)
+        pub.publish(fname)
+        rate.sleep
+
+if __name__ == '__main__':
+    try:
+        firstname_pub()
+    except rospy.ROSInterruptException:
+        pass
