f79e2d37cbc69a877805c5825543777b40876d29
Srujan-D <53463585+Srujan-D@users.noreply.github.com>
2020-05-25 15:30:16 +0530

    Create fullname_sub_pub.py
    This is a third node which will subscribe to the topics ​ /name​ and ​ /surname ​ and
    publish my full name on the topic ​ /fullname.

---
+++ WEEK 2/Solutions/Task 1/scripts/fullname_sub_pub.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+name0 = "a"
+name1 = "b"
+
+def callback0(msg):
+    #rospy.loginfo("%s", msg)
+    global name0 
+    name0 = str(msg.data)
+
+def callback1(msg):
+    #rospy.loginfo("%s", msg)
+    global name1 
+    name1 = str(msg.data)
+
+def full_name():
+    rospy.init_node('fullname_sub_pub', anonymous = True)
+    rospy.Subscriber("name", String, callback0)
+    rospy.Subscriber("surname", String, callback1)
+    pub = rospy.Publisher('fullname', String, queue_size=10)
+    rate = rospy.Rate(0.1) #10Hz
+    #rospy.spin()
+
+    while not rospy.is_shutdown():
+        fullname = name0 + " " + name1
+        rospy.loginfo(fullname)
+        pub.publish(fullname)
+        rate.sleep
+
+if __name__ == '__main__':
+    try:
+        full_name()
+    except rospy.ROSInterruptException:
+        pass
